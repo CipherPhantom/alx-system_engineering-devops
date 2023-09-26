959ef43055e99fce60785294c33c8e812d90ad57
Cipher Phantom <oyepitan10@gmail.com>
2023-09-26 23:05:22 +0100

    7-puppet_install_nginx_web_server.pp_3

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 #This script installs nginx and configures the server
 
 exec { 'Server Config':
-  provider => shell;
+  provider => shell,
   command => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i -r "s/^}$/\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch\?v=QH2-TGUlwu4;\n\t}\n}/" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
