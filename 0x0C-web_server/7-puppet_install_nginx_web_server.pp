eb5ae8b3a3995b5bf24a0a8251a7c62a5a88d904
Cipher Phantom <oyepitan10@gmail.com>
2023-09-26 22:58:25 +0100

    7-puppet_install_nginx_web_server.pp_1

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,15 +1,6 @@
 #This script installs nginx and configures the server
 
-package { 'nginx':
-  ensure => 'installed',
-}
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\troot /var/www/html;\n\n\tserver_name _;\n\n\tindex index.html index.htm index.nginx-debian.html;\n\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n}",
+exec { 'Server Config':
+  provider => shell;
+  command => 'apt-get -y update; apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sed -i -r "s/^}$/\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch\?v=QH2-TGUlwu4;\n\t}\n}/" /etc/nginx/sites-available/default; service nginx restart',
 }
