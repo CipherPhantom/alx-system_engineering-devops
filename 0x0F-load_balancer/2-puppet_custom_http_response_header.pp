7d7f3f4c8c0dd23331cf8ca63367aeac436600c8
Cipher Phantom <oyepitan10@gmail.com>
2023-10-02 15:13:25 +0100

    2-puppet_custom_http_response_header.pp

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# This script installs nginx and configures the server
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+}
+
+exec { 'redirect_me':
+  provider => shell,
+  command  => 'sed -i -r "s/^}$/\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch\?v=QH2-TGUlwu4;\n\t}\n}/" /etc/nginx/sites-available/default',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page",
+}
+
+exec { '404-url':
+  provider => shell,
+  command  => 'sed -i -r "s/^}$/\n\terror_page 404 \/404.html;\n\tlocation \/404.html {\n\t\tinternal;\n\t}\n}/" /etc/nginx/sites-available/default',
+}
+
+exec { 'custom-header':
+  provider => shell,
+  command  => 'sed -i "/^\troot/i \ \tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
