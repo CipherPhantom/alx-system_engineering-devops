70450fdce20ad9a64b6cd99ca3dce76206c236b1
Cipher Phantom <oyepitan10@gmail.com>
2023-10-02 15:57:18 +0100

    Task-2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,25 @@
 # This script installs nginx and configures the server
 
+exec { 'apt update':
+  command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => 'installed',
-}
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => 'Hello World!',
-}
-
-exec { 'sed':
-  provider => shell,
-  command  => 'sed -i -r "s/^}$/\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/www.youtube.com\/watch\?v=QH2-TGUlwu4;\n\t}\n}/" /etc/nginx/sites-available/default',
-}
-
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page",
-}
-
-exec { 'sed_1':
-  provider => shell,
-  command  => 'sed -i -r "s/^}$/\n\terror_page 404 \/404.html;\n\tlocation \/404.html {\n\t\tinternal;\n\t}\n}/" /etc/nginx/sites-available/default',
-}
-
-exec { 'sed_2':
-  provider => shell,
-  command  => 'sed -i "/^\troot/i \ \tadd_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  ensure  => 'installed',
+  require => Exec['apt update'],
 }
 
 service { 'nginx':
   ensure => 'running',
   enable => true,
+}
+
+file_line { 'http header':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  line     => '\tlocation / {\n\t\tadd_header X-Served-By $hostname;',
+  match    => '^\tlocation / {',
+  multiple => false,
+  require  => Package['nginx'],
+  notify   => Service['nginx'],
 }
